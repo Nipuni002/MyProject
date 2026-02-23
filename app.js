7310c23075e1c64f16d063857e23424e51000d7c
Nipuni Perera <nipunithakshilaperera@gmail.com>
2026-02-23 09:36:03 +0530

    Added write file feature

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Node.js project setup successful!");
+
+const fs = require('fs');
+
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+    if (err) throw err;
+    console.log('File saved!');
+});
